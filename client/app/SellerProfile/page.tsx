304279e733c71044afcb316521d505292ea7d751
Mohamed9820m <mohamedhabibmarouani8@gmail.com>
2023-07-19 21:22:59 +0100

    done

---
+++ client/app/SellerProfile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <div className="BackgroundImage"></div>
+      <div className="CoverImage"></div>
+    </div>
+  );
+};
+
+export default Index;
