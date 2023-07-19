1a7658270be9c55b13cf3263306bc51e176e55be
TesnimDrissi <drissitesnime8@gmail.com>
2023-07-19 13:38:00 +0100

    changes

--- server/model/Order.js
+++ server/model/Order.js
@@ -11,6 +11,10 @@ const Order = sequelize.define('Order', {
     type: DataTypes.FLOAT,
     allowNull: false,
   },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 Order.belongsTo(User);
