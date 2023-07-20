01b4ef50bb088486604f8be50e985ce8b720ab32
SouhailAloui <alouisouhail227@gmail.com>
2023-07-20 12:49:35 +0200

    done

--- server/router/admin.js
+++ server/router/admin.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const AdminControler = require('../controllers/admin')
 
-router.get("/", /*  middleware ,controller */);
+router.get("/sellers",AdminControler.getAllseller);
+router.get("/clients",AdminControler.getAllclient);
+router.get("/getAllProduct",AdminControler.getAllProducts);
+router.get("/selectClientCount",AdminControler.SelectclientCount);
+router.get("/selectSellerCount",AdminControler.SelectsellerCount);
 
+
+router.delete("/deleteSeller/:id",AdminControler.deleteSeller)
+router.delete("/deleteClient/:id",AdminControler.deleteClient)
+router.delete("/deleteProduct/:id",AdminControler.deleteProduct)
 
 module.exports = router;
