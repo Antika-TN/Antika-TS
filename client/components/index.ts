35a16ab059825dffefdc17b0d3594eab50e0f26c
SouhailAloui <alouisouhail227@gmail.com>
2023-07-20 19:42:33 +0200

    3th

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,6 +1,8 @@
 import ProdactCard from "./productCard/ProdactCard"
 import AllProdacts from "./AllProdacts/AllProdacts"
+import DashboardTable from "./adminComponents/table"
 export{
     ProdactCard,
-    AllProdacts
+    AllProdacts,
+    DashboardTable
 }
