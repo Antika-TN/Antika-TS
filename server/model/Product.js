da27157579b5fc69643bbc3dace02fbe90cef089
fares boutriga <boutrigafares@gmail.com>
2023-07-20 10:21:37 +0100

    update the prodacts

--- server/model/Product.js
+++ server/model/Product.js
@@ -19,6 +19,10 @@ const Product = sequelize.define('Product', {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   Category:{
     type:DataTypes.ENUM (['Furniture','Decor','Accessories','Vintage ','Tools'])
   }
