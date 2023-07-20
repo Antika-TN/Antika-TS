a6fbd0c0c3cdf78fc98f024c290a59153412c96c
TesnimDrissi <drissitesnime8@gmail.com>
2023-07-20 11:31:06 +0100

    done

--- server/router/orders.js
+++ server/router/orders.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const OrderController= require ("../controllers/orders")
 
 
 
-router.get('/', /*  middleware ,controller */);
+router.get('/:UserId',  /*  middleware ,controller */OrderController.getAll);
+router.get('/:UserId/:id',  /*  middleware ,controller */OrderController.getOne);
+router.post('/creation/:UserId',  /*  middleware ,controller */OrderController.createOrder);
+router.put('/:id',  /*  middleware ,controller */OrderController.updateOrder);
+router.delete('/:id',  /*  middleware ,controller */OrderController.deleteOrder);
 
 
 
