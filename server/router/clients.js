bf0fd68d8da6f716907489835f2be6d47efb07de
TesnimDrissi <drissitesnime8@gmail.com>
2023-07-20 10:32:46 +0100

    changes

--- server/router/clients.js
+++ server/router/clients.js
@@ -1,9 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
+const ClientController= require ("../controllers/clients")
 
 
-router.get('/',  /*  middleware ,controller */);
+router.get('/',  /*  middleware ,controller */ClientController.getAll);
+router.get('/:id',  /*  middleware ,controller */ClientController.getOne);
+router.put('/:id',  /*  middleware ,controller */ClientController.updateClient);
+router.delete('/:id',  /*  middleware ,controller */ClientController.deleteClient);
+
 
 
 
